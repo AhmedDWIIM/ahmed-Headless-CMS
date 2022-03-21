8927a9052b048da90d8107483aa269df88c1fcd7
AhmedDWIIM <91135844+AhmedDWIIM@users.noreply.github.com>
2022-03-21 16:42:22 +0100

    Signing form process completed

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    <h1>Sign In</h1>
+    <h2>Sign In</h2>
+<div id="form">
+  <!-- Email input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="form2Example1">Email address</label>
+    <input v-model="email" type="email" id="form2Example1" class="form-control" />
   </div>
+
+  <!-- Password input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="form2Example2">Password</label>
+    <input v-model="password" type="password" id="form2Example2" class="form-control" />
+  </div>
+
+  <!-- 2 column grid layout for inline styling -->
+
+  <!-- Submit button -->
+  <button @click="loginUser()" type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+</div>
+</div>
+
 </template>
 <script>
-
 export default {
-    name:"signIn",
-  };
+    data(){
+        return{
+            email: "",
+            password: "",
+        }
+    },
+    name:"signUp",
+    methods:{
+        async loginUser(){
+            const {data: user, error} = await this.$supabase.auth.signIn({
+                email:this.email,
+                password:this.password,
+            })
+            if(user){
+              console.log(user);
+            }
+            else if(error){
+              console.log(error);
+            }
+        }
+    }
+}
 </script>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,23 +1,48 @@
 <template>
   <div>
-    <h1>Sign Up</h1>
-    <button type="button" id="btn" class="btn btn-primary" @click="createUser()">Create User</button>
+    <h2>Sign Up</h2>
+<div id="form">
+  <!-- Email input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="form2Example1">Email address</label>
+    <input v-model="email" type="email" id="form2Example1" class="form-control" />
   </div>
+
+  <!-- Password input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="form2Example2">Password</label>
+    <input v-model="password" type="password" id="form2Example2" class="form-control" />
+  </div>
+
+  <!-- 2 column grid layout for inline styling -->
+
+  <!-- Submit button -->
+  <button @click="createUser()" type="button" class="btn btn-primary btn-block mb-4">Sign up</button>
+
+</div>
+</div>
+
 </template>
 <script>
 export default {
+    data(){
+        return{
+            email: "",
+            password: "",
+        }
+    },
     name:"signUp",
     methods:{
         async createUser(){
             const {data: user, error} = await this.$supabase.auth.api.createUser({
-                email:'a@gmail.com',
-                password:'1234',
+                email:this.email,
+                password:this.password,
             })
             if(user){
-              consol.log(user);
+              console.log(user);
             }
             else if(error){
-              consol.log(error);
+              console.log(error);
             }
         }
     }

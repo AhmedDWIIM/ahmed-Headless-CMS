24d8438c761d583afbe7753dbe6e3205cfb3cc86
AhmedDWIIM <91135844+AhmedDWIIM@users.noreply.github.com>
2022-03-21 14:36:11 +0100

    ADD SIGNING BUTTON

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,8 +3,8 @@
 <div class="container-fluid">
     <a class="navbar-brand">Navbar</a>
     <form class="d-flex">
-      <button class="btn btn-outline-success" type="submit">Sign up</button>
-      <button class="btn btn-outline-success" type="submit">Sign in</button>
+      <button type="button" class="btn btn-dark">Sign up</button>
+      <button type="button" class="btn btn-dark">Sign in</button>
     </form>
   </div>
 </nav>
